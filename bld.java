9850f8ab0bf8952df07539aa122744320bac55b2
grfrost <frost.gary@gmail.com>
2025-07-07 18:13:06 +0100

    cleaned up bld

--- bld.java
+++ bld.java
@@ -2,10 +2,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import job.Job;
 
-    public static void main(String[] argArr) throws IOException, InterruptedException {
-        Path userDir = Path.of(System.getProperty("user.dir"));
-        var project = new Job.Project(userDir.getFileName().toString().equals("intellij") ? userDir.getParent() : userDir, new Job.Reporter());
-        var core = Job.Jar.of(project.id("core"));
-        project.build(List.of());
-    }
+void main(String[] argArr) throws IOException, InterruptedException {
+   var project = new Job.Project( Path.of(System.getProperty("user.dir")), Job.Reporter.progressAndErrors);
+   var core = Job.Jar.of(project.id("core"));
+   project.build(List.of());
+}
 
