93584ea5f26943b73cd194d06126bf15fc83da9a
grfrost <frost.gary@gmail.com>
2025-07-25 11:25:26 +0100

    close

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,5 +1,5 @@
 rm -rf build
-javac -d build/bootstrap.classes --source-path core/src/main/java core/src/main/java/job/Job.java
+javac -d build/bootstrap.classes --source-path core/src/main/java $(find core/src/main/java/job -name "*.java")
 jar cf bootstrap.jar -C build/bootstrap.classes job
 java -cp bootstrap.jar scripts/bld.java 
 rm -rf build/bootstrap.classes bootstrap.jar
